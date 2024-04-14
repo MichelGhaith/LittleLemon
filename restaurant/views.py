b8c9fa52cfa96c39dcd89dd78de0f73c718c54c1
unknown <ghaithmichel@gmail.com>
2024-04-14 19:13:32 +0300

    push authenticated check

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,6 +5,8 @@ from .models import Booking,Menu
 from .serializers import BookingSerializer,MenuSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html' , {})
@@ -12,11 +14,14 @@ def index(request):
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
